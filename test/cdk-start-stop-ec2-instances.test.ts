91573b77ce2fb86dd0ea18f577de741d6081c714
Andres Cubillos <felipe.cubillos@outlook.com>
2024-12-02 17:17:06 -0700

    Initial commit

---
+++ test/cdk-start-stop-ec2-instances.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkStartStopEc2Instances from '../lib/cdk-start-stop-ec2-instances-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-start-stop-ec2-instances-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkStartStopEc2Instances.CdkStartStopEc2InstancesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
