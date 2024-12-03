e18e54c7aeb80d25f6e21bcdc5802bd0e7211d67
Andres Cubillos <felipe.cubillos@outlook.com>
2024-12-02 17:38:17 -0700

    ec2 scheduler code added

--- lib/cdk-start-stop-ec2-instances-stack.ts
+++ lib/cdk-start-stop-ec2-instances-stack.ts
@@ -1,16 +1,58 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Role, ServicePrincipal, ManagedPolicy } from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+import { ParameterNames } from "../properties/parameter-names";
+import { CfnSchedule } from "aws-cdk-lib/aws-scheduler/lib";
 
 export class CdkStartStopEc2InstancesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //👇 Create an IAM role that EventBridge Scheduler can assume to call EC2 API operations.
+    const schedulerRole = new Role(this, "SchedulerRole", {
+      assumedBy: new ServicePrincipal("scheduler.amazonaws.com"),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2FullAccess"),
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStartStopEc2InstancesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 👇 Schedule to start an EC2 instance
+    new CfnSchedule(
+      this,
+      `${ParameterNames.PROJECT_NAME}-EC2-scheduler-start`,
+      {
+        name: `${ParameterNames.PROJECT_NAME}-Start`,
+        flexibleTimeWindow: {
+          mode: "OFF",
+        },
+        scheduleExpression: "cron(30 17 ? * 7#2 *)", // 👈 cron jon expression
+        scheduleExpressionTimezone: "Canada/Mountain", // 👈 Specify the timezone to match your requirements.
+        target: {
+          arn: "arn:aws:scheduler:::aws-sdk:ec2:startInstances", // 👈 Api operation to call
+          roleArn: schedulerRole.roleArn, // 👈 The role with permissions to perform this action.
+          input: JSON.stringify({
+            InstanceIds: [ParameterNames.EC2_INSTANCE_ID], // list of instances ID's to start
+          }),
+        },
+      }
+    );
+
+    // Stop EC2 instance 5 Hours later
+
+    new CfnSchedule(this, `${ParameterNames.PROJECT_NAME}-EC2-scheduler-stop`, {
+      name: `${ParameterNames.PROJECT_NAME}-Stop`,
+      flexibleTimeWindow: {
+        mode: "OFF",
+      },
+      scheduleExpression: "cron(30 22 ? * 7#2 *)",
+      scheduleExpressionTimezone: "Canada/Mountain",
+      target: {
+        arn: "arn:aws:scheduler:::aws-sdk:ec2:stopInstances",
+        roleArn: schedulerRole.roleArn,
+        input: JSON.stringify({
+          InstanceIds: [ParameterNames.EC2_INSTANCE_ID],
+        }),
+      },
+    });
   }
 }
---
+++ properties/parameter-names.ts
@@ -0,0 +1,13 @@
+export class ParameterNames {
+  public static readonly PROJECT_NAME = "eventbridge-scheduler-app";
+  // 👇 Add your destination account number 👇
+  public static readonly ACCOUNT = "";
+  // 👇 Add your destination region 👇
+  public static readonly REGION = "us-west-2";
+  // 👇 Add your source account number 👇
+  public static readonly ORIGIN_ACCOUNT = "";
+  // 👇 Add destination Bucket name   👇
+  public static readonly DESTINATION_BUCKET_NAME = "";
+  // 👇 Add ec2 instances id  👇
+  public static readonly EC2_INSTANCE_ID = "";
+}
